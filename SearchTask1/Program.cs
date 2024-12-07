d1d9bcaf3f61b845447a6db731c57fca9b71eec6
ahmedsalah1232 <ibrahemsalah519@gmail.com>
2024-12-07 19:04:33 +0200

    first commit

---
+++ SearchTask1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SearchTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            bool d = true;
+            try
+            {
+                do
+                {
+                    int n;
+                    Console.Write("Enter your element : ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    if (list.Count == 0)
+                    {
+                        list.Add(n);
+                    }
+                    else
+                    {
+                        for(int i = 0;i<list.Count;i++)
+                        {
+                            if (list[i] == n)
+                            {
+                                d=false;
+                                throw new DuplicateWaitObjectException();
+                            }
+                        }
+                    }
+                    list.Add(n);
+                } while (d == true);
+            }
+            catch(DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SearchTask2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SearchTask2
+{
+    internal class Program
+    {
+        static void ExceptionString(string str)
+        {
+            str = str.ToLower();
+            bool b = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'y')
+                {
+                    b = false;
+                }
+            }
+            if (b == true)
+            {
+                throw new Exception("your string does not have vowels");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            try
+            {
+
+                Console.Write("Enter your string : ");
+                str = (Console.ReadLine());
+                ExceptionString(str);
+                Console.WriteLine(str);
+
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
